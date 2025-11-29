c3683fb014807c93b348b049fb3d45ce5f4126e7
allenlin7 <chinalinzaipeng@gmail.com>
2025-11-29 08:20:55 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["8.163.10.49"];
+pub const RS_PUB_KEY: &str = "cII7FYmMZpQkLkuE06zFE0jIF5Q+Imr2Hy3sJJehKeE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
